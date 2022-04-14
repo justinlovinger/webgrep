c2fa3d6c240baee71659f2aad0458b8980dcaf04
Justin Lovinger <git@justinlovinger.com>
2022-04-14 15:08:24 +0000

    Add initial command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{command, Arg};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let matches = command!()
+        .about("Recursively search the web, starting from URI..., for PHRASE")
+        .arg(
+            Arg::new("PHRASE")
+                .required(true)
+                .help("Phrase to search for"),
+        )
+        .arg(
+            Arg::new("URI")
+                .multiple_occurrences(true)
+                .required(true)
+                .help("URIs to start search from"),
+        )
+        .get_matches();
+
+    println!("{}", matches.value_of("PHRASE").unwrap());
+    println!(
+        "{:?}",
+        matches.values_of("URI").unwrap().collect::<Vec<_>>()
+    );
+
+    Ok(())
 }
