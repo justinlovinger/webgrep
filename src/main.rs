963fe83c4a7de3f5440201c393925bef9edb51e6
Justin Lovinger <git@justinlovinger.com>
2022-04-17 22:36:58 +0000

    Refactor DOM functions with `walk_dom`

--- src/main.rs
+++ src/main.rs
@@ -46,61 +46,67 @@ async fn main() -> Result<(), reqwest::Error> {
 // fn parse_page(body: String) -> (links, lines)
 
 fn inner_text(dom: &RcDom) -> String {
-    fn walk(text: &mut String, handle: &Handle) {
-        match handle.data {
-            NodeData::Text { ref contents } => {
-                text.push_str(contents.borrow().to_string().as_str());
-            }
-            NodeData::Element { ref name, .. } => {
-                // The contents of script tags are invisible,
-                // and shouldn't be searched.
-                if name.local.as_ref() != "script" {
-                    for child in handle.children.borrow().iter() {
-                        walk(text, child);
+    let mut text = String::new();
+    walk_dom(
+        &mut |data| {
+            match data {
+                NodeData::Text { ref contents } => {
+                    text.push_str(contents.borrow().to_string().as_str());
+                }
+                NodeData::Element { ref name, .. } => {
+                    // The contents of script tags are invisible,
+                    // and shouldn't be searched.
+                    if name.local.as_ref() == "script" {
+                        return false;
                     }
                 }
+                _ => {}
             }
-            _ => {
-                for child in handle.children.borrow().iter() {
-                    walk(text, child);
-                }
-            }
-        }
-    }
-
-    let mut text = String::new();
-    walk(&mut text, &dom.document);
+            true
+        },
+        &dom.document,
+    );
     text
 }
 
 // TODO: deduplicate links,
 // maybe return a set.
 fn links(origin: &Url, dom: &RcDom) -> Vec<Url> {
-    fn walk(origin: &Url, links: &mut Vec<Url>, handle: &Handle) {
-        match handle.data {
-            NodeData::Element {
-                ref name,
-                ref attrs,
-                ..
-            } => {
-                if name.local.as_ref() == "a" {
-                    attrs
-                        .borrow()
-                        .iter()
-                        .filter(|x| x.name.local.as_ref() == "href")
-                        .take(1) // An `a` tag shouldn't have more than one `href`
-                        .filter_map(|x| origin.join(&x.value).ok())
-                        .for_each(|x| links.push(x));
+    let mut xs = Vec::new();
+    walk_dom(
+        &mut |data| {
+            match data {
+                NodeData::Element {
+                    ref name,
+                    ref attrs,
+                    ..
+                } => {
+                    if name.local.as_ref() == "a" {
+                        attrs
+                            .borrow()
+                            .iter()
+                            .filter(|x| x.name.local.as_ref() == "href")
+                            .take(1) // An `a` tag shouldn't have more than one `href`
+                            .filter_map(|x| origin.join(&x.value).ok())
+                            .for_each(|x| xs.push(x));
+                    }
                 }
+                _ => {}
             }
-            _ => {}
-        }
+            true
+        },
+        &dom.document,
+    );
+    xs
+}
+
+fn walk_dom<F>(f: &mut F, handle: &Handle)
+where
+    F: FnMut(&NodeData) -> bool,
+{
+    if f(&handle.data) {
         for child in handle.children.borrow().iter() {
-            walk(origin, links, child);
+            walk_dom(f, child);
         }
     }
-
-    let mut xs = Vec::new();
-    walk(origin, &mut xs, &dom.document);
-    xs
 }
