8bc6980871bed84b7a5123bea0c00e51b1326276
Justin Lovinger <git@justinlovinger.com>
2022-04-17 15:34:54 +0000

    Get links from URI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use clap::{command, Arg};
+use html5ever::tendril::TendrilSink;
+use markup5ever_rcdom::{Handle, NodeData, RcDom};
+use std::default::Default;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
@@ -20,9 +23,43 @@ async fn main() -> Result<(), reqwest::Error> {
     let phrase = matches.value_of("PHRASE").unwrap();
 
     for u in matches.values_of("URI").unwrap() {
-        let body = reqwest::get(u).await?.text().await?;
-        println!("{}", body.contains(phrase));
+        // TODO: sleep between requests,
+        // see `tokio::time::delay_for`.
+        let dom = html5ever::parse_document(RcDom::default(), Default::default())
+            .from_utf8()
+            .read_from(&mut reqwest::get(u).await?.text().await?.as_bytes())
+            .unwrap();
+        println!("{:?}", links(u, &dom));
     }
 
     Ok(())
+}
+
+fn links(origin: &str, dom: &RcDom) -> Vec<String> {
+    fn walk(links: &mut Vec<String>, handle: &Handle) -> () {
+        match handle.data {
+            NodeData::Element {
+                ref name,
+                ref attrs,
+                ..
+            } => {
+                if name.local.as_ref() == "a" {
+                    attrs
+                        .borrow()
+                        .iter()
+                        .filter(|x| x.name.local.as_ref() == "href")
+                        .take(1) // An `a` tag shouldn't have more than one `href`
+                        .for_each(|x| links.push(x.value.to_string()));
+                }
+            }
+            _ => {}
+        }
+        for child in handle.children.borrow().iter() {
+            walk(links, child);
+        }
+    }
+
+    let mut xs = Vec::new();
+    walk(&mut xs, &dom.document);
+    xs
 }
