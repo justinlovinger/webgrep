77d403295f37d7f9103353897b598258c9f597aa
Justin Lovinger <git@justinlovinger.com>
2022-04-30 16:01:38 +0000

    Reorder code

--- src/main.rs
+++ src/main.rs
@@ -20,75 +20,6 @@ use url::Host::{Domain, Ipv4, Ipv6};
 
 const CLEAR_CODE: &[u8] = b"\r\x1B[K";
 
-pub struct Node<T> {
-    depth: u64,
-    parent: Option<Arc<Node<T>>>,
-    value: T,
-}
-
-pub struct NodePathIterator<'a, T> {
-    node: Option<&'a Arc<Node<T>>>,
-}
-
-impl<'a, T> Iterator for NodePathIterator<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let x = self.node?;
-        self.node = x.parent.as_ref();
-        Some(&x.value)
-    }
-}
-
-impl<T> Node<T> {
-    pub fn new(parent: Option<Arc<Node<T>>>, value: T) -> Self {
-        Node {
-            depth: parent.as_ref().map_or(0, |p| p.depth + 1),
-            parent,
-            value,
-        }
-    }
-
-    pub fn depth(&self) -> u64 {
-        self.depth
-    }
-
-    pub fn path_from_root(&self) -> Vec<&T> {
-        match &self.parent {
-            Some(p) => {
-                let mut xs = p.path_from_root();
-                xs.push(&self.value);
-                xs
-            }
-            None => Vec::from([&self.value]),
-        }
-    }
-}
-
-pub fn path_to_root<T>(x: &Arc<Node<T>>) -> NodePathIterator<T> {
-    NodePathIterator { node: Some(x) }
-}
-
-impl<T> Ord for Node<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.depth().cmp(&other.depth())
-    }
-}
-
-impl<T> PartialOrd for Node<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T> Eq for Node<T> {}
-
-impl<T> PartialEq for Node<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.depth() == other.depth()
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let matches = command!()
@@ -334,6 +265,75 @@ where
             xs.swap_remove(i);
         };
     }
+}
+
+pub struct Node<T> {
+    depth: u64,
+    parent: Option<Arc<Node<T>>>,
+    value: T,
+}
+
+impl<T> Node<T> {
+    pub fn new(parent: Option<Arc<Node<T>>>, value: T) -> Self {
+        Node {
+            depth: parent.as_ref().map_or(0, |p| p.depth + 1),
+            parent,
+            value,
+        }
+    }
+
+    pub fn depth(&self) -> u64 {
+        self.depth
+    }
+
+    pub fn path_from_root(&self) -> Vec<&T> {
+        match &self.parent {
+            Some(p) => {
+                let mut xs = p.path_from_root();
+                xs.push(&self.value);
+                xs
+            }
+            None => Vec::from([&self.value]),
+        }
+    }
+}
+
+impl<T> Ord for Node<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.depth().cmp(&other.depth())
+    }
+}
+
+impl<T> PartialOrd for Node<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T> Eq for Node<T> {}
+
+impl<T> PartialEq for Node<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.depth() == other.depth()
+    }
+}
+
+pub struct NodePathIterator<'a, T> {
+    node: Option<&'a Arc<Node<T>>>,
+}
+
+impl<'a, T> Iterator for NodePathIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let x = self.node?;
+        self.node = x.parent.as_ref();
+        Some(&x.value)
+    }
+}
+
+pub fn path_to_root<T>(x: &Arc<Node<T>>) -> NodePathIterator<T> {
+    NodePathIterator { node: Some(x) }
 }
 
 struct CachingClient {
